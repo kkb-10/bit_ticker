7a6521fd7465e45c10b2141da33d5a5a6bda0fa4
Kirti Kunj Bajpai <kkbajpai.kk@gmail.com>
2020-10-10 01:41:58 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData.dark(),
     );
   }
